42bb4bc6b3fa87e0e94e5c0ebd8f80b774f6b519
techandsoju <46474702+techandsoju@users.noreply.github.com>
2022-11-09 02:40:50 -0800

    Code cleanup

--- src/App.js
+++ src/App.js
@@ -28,29 +28,7 @@ function App() {
     const response = await fetch(`${BASEURL}${ENDPOINTS.PAYMENTS}`);
     const data = await response.json();
 
-    // // Filtering Payment Data
-    // if (searchTerm) {
-
-    //   let paymentUpdateData = [data?.data, ...paymentValues, ...arrayEmptyInitialize];
-    //   let searchTermLowerCase = searchTerm.toString().toLowerCase();
-
-    //   paymentUpdateData = paymentUpdateData.filter((payment) => {
-    //     return (
-    //       payment?.id?.toString().includes(searchTermLowerCase) ||
-    //       payment?.date?.includes(searchTermLowerCase) ||
-    //       payment?.sender?.id?.toString().includes(searchTermLowerCase) ||
-    //       payment?.sender?.name?.toLowerCase().includes(searchTermLowerCase) ||
-    //       payment?.receiver?.id?.toString().includes(searchTermLowerCase) ||
-    //       payment?.receiver?.name?.toLowerCase().includes(searchTermLowerCase) ||
-    //       payment?.amount?.toString().includes(searchTermLowerCase) ||
-    //       payment?.currency?.toString().toLowerCase().includes(searchTermLowerCase) ||
-    //       payment?.memo?.toString().toLowerCase().includes(searchTermLowerCase))
-    //   });
-
-    //   dispatch(setFilterPayments(paymentUpdateData));
-    // }
     dispatch(addPayments(data?.data));
-    // dispatch(filterPayments({filterTerm: searchTerm}));
    
   }, []);
 
